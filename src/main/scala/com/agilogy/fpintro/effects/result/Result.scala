2810d1fe3e1301520212ff56f2b80d592d6abaf0
Jordi Pradel <jordi.pradel@agilogy.com>
2019-11-16 01:36:13 +0100

    Implement ResultMessagesRepository by catching exceptions

--- src/main/scala/com/agilogy/fpintro/effects/result/Result.scala
+++ src/main/scala/com/agilogy/fpintro/effects/result/Result.scala
@@ -5,4 +5,7 @@ sealed trait Result[+A]
 object Result {
   final case class Ok[A](result: A)            extends Result[A]
   final case class Error(exception: Exception) extends Result[Nothing]
+
+  def fromImperative[A](f: () => A): Result[A] = try { Result.Ok(f()) } catch { case e: Exception => Result.Error(e) }
+
 }
---
+++ src/main/scala/com/agilogy/fpintro/messages/infrastructure/ResultMessagesRepository.scala
@@ -0,0 +1,52 @@
+package com.agilogy.fpintro.messages.infrastructure
+
+import java.nio.charset.StandardCharsets
+import java.nio.file.{Files, Paths}
+
+import scala.collection.JavaConverters._
+import scala.util.matching.Regex
+
+import com.agilogy.fpintro.effects.result.Result
+import com.agilogy.fpintro.messages.app.ResultMessagesRepository
+import com.agilogy.fpintro.messages.domain.{Emoji, Message, MessageContent}
+
+object ResultMessagesRepository extends ResultMessagesRepository {
+
+  private val path                 = Paths.get("./messages.txt").toAbsolutePath
+  private val StoredMessage: Regex = s"(.*) -- ?(.*)".r
+
+  override def selectByContent(content: MessageContent): Result[Message] =
+    Result.fromImperative(() => selectAll().find(_.content == content).getOrElse(throw new MessageNotFoundException))
+
+  override def update(message: Message): Result[Unit] =
+    Result.fromImperative(() => {
+      val currentMessages = selectAll()
+      if (!currentMessages.exists(_.content == message.content)) throw new MessageNotFoundException()
+      val newMessages = selectAll().map {
+        case Message(content, _) if content == message.content => message
+        case m                                                 => m
+      }
+      writeMessages(newMessages)
+    })
+
+  private def selectAll(): List[Message] = {
+    val lines = Files.readAllLines(path).asScala
+    lines.map {
+      case StoredMessage(message, reactions) =>
+        Message(MessageContent(message), reactions.split(",").filter(_.nonEmpty).map(Emoji(_)).toList)
+    }.toList
+  }
+
+  private def writeMessages(messages: List[Message]): Unit = {
+    val fileContent = messages
+      .map { message =>
+        val content   = message.content.value
+        val reactions = message.reactions.map(_.value).mkString(",")
+        s"$content -- $reactions"
+      }
+      .mkString("\n")
+    println(s"Saving...\n$fileContent\n")
+    Files.write(path, fileContent.getBytes(StandardCharsets.UTF_8))
+  }
+
+}
