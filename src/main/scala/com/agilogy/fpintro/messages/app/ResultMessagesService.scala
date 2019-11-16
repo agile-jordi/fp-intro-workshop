73c26074cbb745e463317aac449f47944a8549ba
Jordi Pradel <jordi.pradel@agilogy.com>
2019-11-16 01:39:40 +0100

    Try to implement ResultMessagesService by composing 2 Result programs
    It does not compile, since composition is not longer just appending
    one program after the other.

---
+++ src/main/scala/com/agilogy/fpintro/messages/app/ResultMessagesService.scala
@@ -0,0 +1,12 @@
+package com.agilogy.fpintro.messages.app
+
+import com.agilogy.fpintro.messages.domain.{Emoji, MessageContent}
+
+final class ResultMessagesService(repository: ResultMessagesRepository) {
+
+  def addReactions(): Unit = {
+    val goodMorning = MessageContent("Good mooorning FP laaand!")
+    val message     = repository.selectByContent(goodMorning)
+    repository.update(message.addReaction(Emoji.Sun).addReaction(Emoji.Tada))
+  }
+}
