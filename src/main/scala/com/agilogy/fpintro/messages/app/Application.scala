ae824478ba7931fde54cc764956a994eb95c8cf1
Jordi Pradel <jordi.pradel@agilogy.com>
2019-11-16 01:22:33 +0100

    Inject the repository to use to the function that needs it

--- src/main/scala/com/agilogy/fpintro/messages/app/Application.scala
+++ src/main/scala/com/agilogy/fpintro/messages/app/Application.scala
@@ -1,7 +1,10 @@
 package com.agilogy.fpintro.messages.app
 
+import com.agilogy.fpintro.messages.infrastructure.MessagesRepository
+
 object Application {
 
-  def main(args: Array[String]): Unit = MessagesService.addReactionsToGoodMorning()
+  private val repository: MessagesRepository = MessagesRepository
+  def main(args: Array[String]): Unit        = MessagesService.addReactionsToGoodMorning(repository)
 
 }
--- src/main/scala/com/agilogy/fpintro/messages/app/MessagesService.scala
+++ src/main/scala/com/agilogy/fpintro/messages/app/MessagesService.scala
@@ -1,14 +1,13 @@
 package com.agilogy.fpintro.messages.app
 
 import com.agilogy.fpintro.messages.domain.{Emoji, MessageContent}
-import com.agilogy.fpintro.messages.infrastructure.MessagesRepository
 
 object MessagesService {
 
-  def addReactionsToGoodMorning(): Unit = {
+  def addReactionsToGoodMorning(repository: MessagesRepository): Unit = {
     val goodMorning = MessageContent("Good mooorning FP laaand!")
-    val message     = MessagesRepository.selectByContent(goodMorning)
-    MessagesRepository.update(message.addReaction(Emoji.Sun).addReaction(Emoji.Tada))
+    val message     = repository.selectByContent(goodMorning)
+    repository.update(message.addReaction(Emoji.Sun).addReaction(Emoji.Tada))
   }
 
 }
