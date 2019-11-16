da3fdd01356f0bbec5c3d69e63f4da609144c3c7
Jordi Pradel <jordi.pradel@agilogy.com>
2019-11-16 02:53:16 +0100

    Compose the Async programs with a new function andThen

--- src/main/scala/com/agilogy/fpintro/effects/async/Async.scala
+++ src/main/scala/com/agilogy/fpintro/effects/async/Async.scala
@@ -1,3 +1,5 @@
 package com.agilogy.fpintro.effects.async
 
-final case class Async[A](run: () => A)
+final case class Async[A](run: () => A) {
+  def andThen[B](f: A => Async[B]): Async[B] = Async(() => f(this.run()).run())
+}
--- src/main/scala/com/agilogy/fpintro/messages/app/AsyncMessagesService.scala
+++ src/main/scala/com/agilogy/fpintro/messages/app/AsyncMessagesService.scala
@@ -7,8 +7,9 @@ final class AsyncMessagesService(repository: AsyncMessagesRepository) {
 
   def addReactionsToGoodMorning(): Async[Unit] = {
     val goodMorning = MessageContent("Good mooorning FP laaand!")
-    val message     = repository.selectByContent(goodMorning)
-    repository.update(message.addReaction(Emoji.Sun).addReaction(Emoji.Tada))
+    repository.selectByContent(goodMorning).andThen { message =>
+      repository.update(message.addReaction(Emoji.Sun).addReaction(Emoji.Tada))
+    }
   }
 
 }
