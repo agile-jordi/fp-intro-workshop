880d2a77e0a785cbab6fa92db48ef90aaf789df4
Jordi Pradel <jordi.pradel@agilogy.com>
2019-11-19 09:42:31 +0100

    Use a Scala for comprehension in MessagesService

--- src/main/scala/com/agilogy/fpintro/messages/app/MessagesService.scala
+++ src/main/scala/com/agilogy/fpintro/messages/app/MessagesService.scala
@@ -13,9 +13,12 @@ final class MessagesService[F[_]](repository: MessagesRepository[F])(implicit ca
     // those in Any (that we don't want to use anyway) and those added by some syntax (see CanContinueSyntax).
     // In case of getting lost by implicits, remember to use View > Show implicit hints in Intellij (and View ->
     // Expand Implicit Hints if needed)
-    repository
-      .selectByContent(goodMorning)
-      .flatMap(m => repository.update(m.addReaction(Emoji.Sun).addReaction(Emoji.Tada)))
+    // The for comprehension is Scala syntax sugar for flatMap and map
+    for {
+      m   <- repository.selectByContent(goodMorning)
+      res <- repository.update(m.addReaction(Emoji.Sun).addReaction(Emoji.Tada))
+    } yield res
+
   }
 
 }
